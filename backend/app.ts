b622ab0d7c8725a78d5e74705b428ce488cfaf04
Aaron Kang <aaronkang612@gmail.com>
2021-11-22 03:13:10 -0500

    backend skeleton

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,30 @@
 import express from 'express';
+import admin from 'firebase-admin';
+import cors from 'cors';
+import "firebase/auth";
+import * as dotenv from "dotenv";
 
+import clubRoutes from "./router/club";
+import studentRoutes from "./router/student"
+
+dotenv.config();
+
+admin.initializeApp({
+  //TO BE REPLACED LATER
+  // apiKey: "API_KEY",
+  // authDomain: "PROJECT_ID.firebaseapp.com",
+  // databaseURL: "https://PROJECT_ID.firebaseio.com",
+  // projectId: "PROJECT_ID",
+  // storageBucket: "PROJECT_ID.appspot.com",
+  // messagingSenderId: "SENDER_ID",
+  // appId: "APP_ID",
+  // measurementId: "G-MEASUREMENT_ID",
+})
+const db = admin.firestore();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Well done!');
-});
-
-app.listen(3000, () => {
-  console.log('The application is listening on port 3000!');
-});
+app.use(cors({ origin: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/club", clubRoutes);
+app.use("/student", studentRoutes);
