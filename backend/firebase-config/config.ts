f7c7b64589cd51f7dc1df3ea68096e6e30746531
Aaron Kang <aaronkang612@gmail.com>
2021-12-01 02:12:18 -0500

    configuring firebase

---
+++ backend/firebase-config/config.ts
@@ -0,0 +1,14 @@
+import admin from 'firebase-admin';
+import { config } from 'dotenv';
+
+config();
+
+const serviceAccount = require('../service-account.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore();
+
+export { db };
