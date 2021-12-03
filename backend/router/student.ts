6f66b14fa40e4fe9bd012aa6e932ec115ce17264
Austin Wu <hw424@cornell.edu>
2021-12-03 12:45:43 -0500

    Formatted

--- backend/router/student.ts
+++ backend/router/student.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/getUser/all', (req, res) => {
-  res.send("Getting all students...")
-}); 
+  res.send('Getting all students...');
+});
 
 router.get('/getUser/{id}', (req, res) => {
-  res.send("Getting student...")
-}); 
+  res.send('Getting student...');
+});
 
 export default router;
