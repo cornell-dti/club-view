e3dc34e1e011f762bd714a3c2384fb8f3ffcd9a0
Austin Wu <hw424@cornell.edu>
2021-12-05 15:17:03 -0500

    Init

--- backend/router/club.ts
+++ backend/router/club.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Getting Clubs...');
+router.get('/club/all', (req, res) => {
+  res.send('Getting all clubs...');
+});
+
+router.get('/club/{id}', (req, res) => {
+  res.send('Getting club...');
 });
 
 export default router;
