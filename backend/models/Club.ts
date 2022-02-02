3206109f24a816daa81df40704d856dab33418c7
Justin Kang <68215015+justinkang01@users.noreply.github.com>
2022-02-01 20:07:05 -0500

    Created initial types

--- backend/models/Club.ts
+++
@@ -1,9 +0,0 @@
-export type ClubType = {
-  id: string;
-  name: string;
-  managingStudent: string;
-  category: string;
-  status: string;
-  openDate: string;
-  closeDate: string;
-};
--- backend/types/types.ts
+++ backend/types/types.ts
@@ -1,1 +1,54 @@
 //file for types
+export type StudentType = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  year: string;
+  email: string;
+  managed: string[];
+  favorites: string[];
+};
+
+export type ClubType = {
+  id: string;
+  name: string;
+  category: string;
+  email: string;
+  description: string;
+  url: string;
+  status: string;
+  openDate?: string;
+  closeDate?: string;
+  registeredBy: string;
+};
+
+export type EventType = {
+  id: string;
+  name: string;
+  clubID: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  location: string;
+};
+
+export enum CategoryType {
+  ACADEMIC = 'academic',
+  PROJECTTEAM = 'project team',
+  CAREERDEVELOPMENT = 'careerdevelopment',
+  COMMUNITYSERVICE = 'communityservice',
+  CULTURAL = 'cultural',
+  SOCIAL = 'social',
+  FOOD_DRINK = 'food and drink',
+  HEALTH_WELLBEING = 'health and wellbeing',
+  GAMING = 'gaming',
+  POLITICAL_ADVOCACY = 'political and advocacy',
+  PROFESSIONAL_FRATERNITY_COUNCIL = 'professional fraternity council',
+  PUBLICATION_MEDIA = 'publication and media',
+  SPIRITUAL_RELIGIOUS = 'spiritual and religious',
+  SPORTS_CLUB_COUNCIL = 'sports club council',
+  STUDENT_GOVERNMENT = 'student government',
+  SUSTAINABILITY = 'sustainability',
+  ENVIRONMENTAL_WILDLIFE = 'environmental and wildlife',
+  THE_ARTS = 'the arts',
+}
