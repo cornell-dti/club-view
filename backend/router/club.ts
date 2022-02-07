1497cfe45bd3b0ca71def45f8bfe5eb5ddaaa53a
Aaron Kang <aaronkang612@gmail.com>
2022-02-06 23:29:14 -0500

    added endpoints and cleaned up some code.

--- backend/router/club.ts
+++ backend/router/club.ts
@@ -1,23 +1,32 @@
 import express from 'express';
+import {ClubType} from '../types/types'
 import db from '../firebase-config/config';
 
 const router = express.Router();
-
-type Club = {
-  name: string;
-};
 
 //Gets all club infrormation
 router.get('/', async (req, res) => {
   const clubsCollection = db.collection('clubs');
   const clubsSnapshot = await clubsCollection.get();
   const allClubs = clubsSnapshot.docs;
-  const clubs: Club[] = [];
+  const clubs: ClubType[] = [];
   for (let doc of allClubs) {
-    const post: Club = doc.data() as Club;
-    clubs.push(post);
+    const club: ClubType = doc.data() as ClubType;
+    clubs.push(club);
   }
   res.send(clubs);
 });
+
+router.get('/:id', async (req, res)=>{
+  const clubId = req.params.id;
+  const clubsCollection = db.collection('clubs');
+  const ref = clubsCollection.doc(clubId);
+  const doc = await ref.get();
+  if (!doc.exists) {
+    throw new Error('Invalid id');
+  }
+  const data = doc.data() as ClubType;
+  res.send(data);
+})
 
 export default router;
