d9cbb797fecf3b1075979973db38f0b5403f28fa
Justin <justinkong24@gmail.com>
2022-02-14 15:21:34 -0500

    added components of nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ClubBoard from './components/ClubBoard/ClubBoard';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <ClubBoard />
+      <NavBar />
     </div>
   );
 }
