4019a6fe8a9f5610a2b315fb9669d1fb28bb3470
Aaron Kang <aaronkang612@gmail.com>
2022-02-22 02:34:32 -0500

    lint rule changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
         tsx: 'never',
       },
     ],
+    "import/no-default-export": "error",
     'linebreak-style': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
