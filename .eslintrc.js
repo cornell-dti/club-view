ff1ae508fe340b19f88a289fea715c3aeed1c8ef
Aaron Kang <aaronkang612@gmail.com>
2022-02-22 03:15:07 -0500

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
         tsx: 'never',
       },
     ],
-    "import/no-default-export": "error",
+    'import/no-default-export': 'error',
     'linebreak-style': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
