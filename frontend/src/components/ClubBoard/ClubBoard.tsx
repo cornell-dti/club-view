66f4e137b4726302d9de0ac65cbd7a02211fa8aa
Daniel Wei <dlw266@cornell.edu>
2022-02-22 20:35:05 -0500

    Display the Club Data from Backend

--- frontend/src/components/ClubBoard/ClubBoard.tsx
+++ frontend/src/components/ClubBoard/ClubBoard.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClubCard from '../ClubCard/ClubCard';
 import { ClubType } from '../../../../backend/types/types';
 import './ClubBoard.css';
 
 const ClubBoard = () => {
-  let clubArray: ClubType[] = new Array(100).fill({
-    name: 'Club Name',
-    category: 'Category',
-  });
+  // let clubArray: ClubType[] = new Array(100).fill({
+  //   name: 'Club Name',
+  //   category: 'Category',
+  // });
+
+  const [clubArray, setClubArray] = useState<ClubType[]>([]);
+
+  fetch('http://localhost:8000/clubs')
+    .then((res) => res.json())
+    .then((data) => setClubArray(data))
+    .then(() => console.log(clubArray));
+
   return (
     <div className="cardsContainer">
       {clubArray.map((club) => (
