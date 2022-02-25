6a360741b35d9a141a6e82931e67390e51fbad7c
Daniel Wei <dlw266@cornell.edu>
2022-02-24 22:05:31 -0500

    Load Clubs on Component Mount
    via useEffect
    
    Also, Log ClubArray on Load from Backend

--- frontend/src/components/ClubBoard/ClubBoard.tsx
+++ frontend/src/components/ClubBoard/ClubBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ClubCard from '../ClubCard/ClubCard';
 import { ClubType } from '../../../../backend/types/types';
 import './ClubBoard.css';
@@ -11,10 +11,22 @@ const ClubBoard = () => {
 
   const [clubArray, setClubArray] = useState<ClubType[]>([]);
 
-  fetch('http://localhost:8000/clubs')
-    .then((res) => res.json())
-    .then((data) => setClubArray(data))
-    .then(() => console.log(clubArray));
+  // This useEffect is triggered on render
+  useEffect(() => {
+    if (clubArray.length !== 0) {
+      console.log('Club Array Loaded!');
+      console.log(clubArray);
+    }
+  });
+
+  // This useEffect is triggered only on component mount
+  useEffect(() => {
+    // Assuming the backend is already running before starting the frontend,
+    // we should load clubs on mount rather than on render.
+    fetch('http://localhost:8000/clubs')
+      .then((res) => res.json())
+      .then((data) => setClubArray(data));
+  }, []);
 
   return (
     <div className="cardsContainer">
