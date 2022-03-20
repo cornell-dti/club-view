84eff20c9cbf5bc33b0c41a2d85b9e927436a3c9
Aaron Kang <aaronkang612@gmail.com>
2022-03-20 18:49:02 -0400

    formatting

--- frontend/src/components/Tags/SocialTags/SocialTag.tsx
+++ frontend/src/components/Tags/SocialTags/SocialTag.tsx
@@ -1,34 +1,37 @@
-import React from "react";
-import { instragramIcon, facebookIcon, listservIcon, globeIcon } from "../../../icons/tags";
-import "./SocialTag.css"
+import React from 'react';
+import {
+  instragramIcon,
+  facebookIcon,
+  listservIcon,
+  globeIcon,
+} from '../../../icons/tags';
+import './SocialTag.css';
 
-type Prop  = {
+type Prop = {
   socialName: string;
-}
+};
 
-const SocialTag = (props : Prop) =>{ 
-
-  const renderIcon = (socialName: string) =>{
+const SocialTag = (props: Prop) => {
+  const renderIcon = (socialName: string) => {
     const nameParsed = props.socialName.toLowerCase();
-    switch(nameParsed){
-      case "instagram":
+    switch (nameParsed) {
+      case 'instagram':
         return instragramIcon;
-      case "facebook":
+      case 'facebook':
         return facebookIcon;
-      case "listserv":
+      case 'listserv':
         return listservIcon;
-      case "globe":
+      case 'globe':
         return globeIcon;
     }
-  }
+  };
 
   return (
     <div className="socialTag">
       <img className="socialIcon" src={renderIcon(props.socialName)} />
       <div className="socialName">{props.socialName}</div>
     </div>
-
-  )
-}
+  );
+};
 
 export default SocialTag;
