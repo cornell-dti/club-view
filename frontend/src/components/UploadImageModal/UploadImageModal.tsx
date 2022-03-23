5a4cd8070f9266a3b8582a1863185d6763746989
Daniel Wei <dlw266@cornell.edu>
2022-03-23 19:43:14 -0400

    finished styling

--- frontend/src/components/UploadImageModal/UploadImageModal.tsx
+++ frontend/src/components/UploadImageModal/UploadImageModal.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './UploadImageModal.css';
+import { xButton, imgIcon, cloud } from '../../icons/image-modal';
 
 // Required props: whether to show the component or not, and a callback to notify the parent component to close the component
 interface RequiredProps {
@@ -24,18 +25,25 @@ function getFileExt(fname: string) {
   return fname.slice(((fname.lastIndexOf('.') - 1) >>> 0) + 2).toLowerCase();
 }
 
+function isAnImage(file: string) {
+  return 'jpg png jpeg gif'.includes(getFileExt(file));
+}
+
 const UploadImageModal = (props: Props) => {
   const [selectedFile, setSelectedFile] = useState<File>();
 
-  let defaultMessage = 'Drag files here \n or';
+  let defaultMessage = '';
   let tooManyFilesMsg = 'Only one file at a time please!';
-  let notAnImgMsg = "This file isn't an image!";
-  let notAFileMsg = "This isn't a valid file.";
+  let notAnImgMsg = "This file isn't a valid image!";
+  let notAFileMsg = "This isn't a valid file!";
   const [message, setMessage] = useState(defaultMessage);
+
+  const [hoveringDrop, setHoveringDrop] = useState(false);
 
   function updateFile(file: File | undefined) {
     setSelectedFile(file);
     setMessage(defaultMessage);
+    setHoveringDrop(false);
   }
 
   const closeModal = (sendFile: boolean) => {
@@ -54,7 +62,12 @@ const UploadImageModal = (props: Props) => {
   };
 
   const changeHandler = (event: any) => {
-    updateFile(event.target.files[0]);
+    if (isAnImage(event.target.files[0].name)) {
+      updateFile(event.target.files[0]);
+    } else {
+      setMessage(notAnImgMsg);
+      setHoveringDrop(false);
+    }
   };
 
   const handleDrop = (ev: any) => {
@@ -67,22 +80,26 @@ const UploadImageModal = (props: Props) => {
       // Use DataTransferItemList interface to access the file(s)
       if (ev.dataTransfer.items.length > 1) {
         setMessage(tooManyFilesMsg);
+        setHoveringDrop(false);
       } else {
         if (ev.dataTransfer.items[0].kind === 'file') {
           var file = ev.dataTransfer.items[0].getAsFile();
-          if ('jpg png jpeg gif'.includes(getFileExt(file.name))) {
+          if (isAnImage(file.name)) {
             updateFile(file);
           } else {
             setMessage(notAnImgMsg);
+            setHoveringDrop(false);
           }
         } else {
           setMessage(notAFileMsg);
+          setHoveringDrop(false);
         }
       }
     } else {
       // Use DataTransfer interface to access the file(s)
       if (ev.dataTransfer.files.length > 1) {
         setMessage(tooManyFilesMsg);
+        setHoveringDrop(false);
       } else {
         updateFile(ev.dataTransfer.files[0]);
       }
@@ -90,9 +107,12 @@ const UploadImageModal = (props: Props) => {
   };
 
   const handleDragOver = (ev: any) => {
-    console.log('File(s) in drop zone');
-
     // TODO: animate the drop zone on hover ??
+    setHoveringDrop(true);
+
+    // setTimeout(function () {
+    //   setHoveringDrop(false);
+    // }, 5000); //turn it back to grey in 5 seconds
 
     // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
@@ -109,24 +129,70 @@ const UploadImageModal = (props: Props) => {
             <a href="#" className="close" onClick={() => closeModal(false)} />
           </div>
           <div className="modalBody">
-            <div
-              className="dropzone"
-              onDrop={handleDrop}
-              onDragOver={handleDragOver}
-            >
-              <p>{message}</p>
-              <input type="file" name="file" onChange={changeHandler} />
+            <div className="fileinfozone">
+              {selectedFile !== undefined ? (
+                <div className="fileinfo">
+                  <div className="fileNameAndIcon">
+                    <div className="fileIconWrapper">
+                      <img
+                        className="fileIcon"
+                        height="20"
+                        src={imgIcon}
+                        alt="Icon"
+                      />
+                    </div>
+                    <p className="filename">{selectedFile.name}</p>
+                  </div>
+                  <div className="fileIconWrapper">
+                    <img
+                      className="closeIcon"
+                      src={xButton}
+                      alt="Close the dialog"
+                      onClick={() => updateFile(undefined)}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <></>
+              )}
             </div>
+            <div className="dropzonewrapper">
+              <div
+                className={hoveringDrop ? 'dropzoneHovering' : 'dropzone'}
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+              >
+                {
+                  // if the message is the default, that means we have no errors. display the default cloud icon:
+                  message === defaultMessage ? (
+                    <div className="infoCloud">
+                      <img
+                        className="cloudIcon"
+                        height="80"
+                        src={cloud}
+                        alt="Icon"
+                      />
+                      <br />
+                      Drag files here <br /> or
+                    </div>
+                  ) : (
+                    // otherwise, display an error message only
+                    <div className="errorMessage">{message}</div>
+                  )
+                }
 
-            {selectedFile !== undefined ? (
-              <div>
-                <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
-                <p>Size in bytes: {selectedFile.size}</p>
+                <label htmlFor="hiddenFileInput" className="browseFiles">
+                  Browse Photos
+                </label>
+                <input
+                  className="hiddenFileInput"
+                  id="hiddenFileInput"
+                  type="file"
+                  name="file"
+                  onChange={changeHandler}
+                />
               </div>
-            ) : (
-              <p>Select a file to show details</p>
-            )}
+            </div>
           </div>
           <div className="modalFooter">
             <button className="doneButton" onClick={() => closeModal(true)}>
