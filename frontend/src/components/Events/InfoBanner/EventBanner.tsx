ea10ca0af576f90ea9e3ea04875665a3095211af
Aaron Kang <aaronkang612@gmail.com>
2022-04-12 17:19:35 -0400

    formatting

--- frontend/src/components/Events/InfoBanner/EventBanner.tsx
+++ frontend/src/components/Events/InfoBanner/EventBanner.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import "./EventBanner.css"
+import React from 'react';
+import './EventBanner.css';
 
 type BannerProps = {
-  date: string,
-  name: string,
-  location: string,
-  startTime: string,
-  endTime: string 
-}
+  date: string;
+  name: string;
+  location: string;
+  startTime: string;
+  endTime: string;
+};
 
-
-
-const EventBanner = ({date, name, location, startTime, endTime} : BannerProps) => {
-  return(
+const EventBanner = ({
+  date,
+  name,
+  location,
+  startTime,
+  endTime,
+}: BannerProps) => {
+  return (
     <div className="bannerContainer">
-        {date} - {name}
-        <br />
-        {location} • {startTime} - {endTime}
+      {date} - {name}
+      <br />
+      {location} • {startTime} - {endTime}
     </div>
-  )
-}
+  );
+};
 
 export default EventBanner;
